ccb43a4aac6c53bb9f4facb8c544edcfa1d57d9f
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-14 15:54:42 +0200

    dorađeno do prošlog stanja

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-import './App.css'
-import { Component } from 'react'
-import { UserClass, UserFunction, UserChildren } from './user'
+import "./App.css";
+import { Component } from "react";
+import { UserClass, UserFunction, UserChildren } from "./user";
 
 const users = [
-  { name: 'Ivan', years: 30, },
-  { name: 'Marko', years: 35 },
-  { name: 'Ana', years: 25 },
+    { name: "Ivan", years: 30 },
+    { name: "Marko", years: 35 },
+    { name: "Ana", years: 25 },
 ];
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>State</h1>
-        <UserClass name={users[0].name} years={users[0].years} />
-        <UserFunction name={users[1].name} years={users[1].years} />
-        <UserChildren name={users[2].name} years={users[2].years}>
-          a hobi mi je programiranje...
-        </UserChildren>
-      </>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [
+                { name: "Ivan", years: 30 },
+                { name: "Marko", years: 35 },
+                { name: "Ana", years: 25 },
+            ],
+            tekst: "Proizvoljan tekst",
+        };
+    }
+
+    uvecajGodine = () => {
+        //koristite map funkciju za iterirat po state
+        const newUsers = users.map((user) => {
+            return { ...user, years: user.years + 1 };
+        });
+        this.setState({users: newUsers});
+    };
+
+    render() {
+        const { users, tekst } = this.state;
+        return (
+            <>
+                <h1>State</h1>
+                <UserClass name={users[0].name} years={users[0].years} />
+                <UserFunction name={users[1].name} years={users[1].years} />
+                <UserChildren name={users[2].name} years={users[2].years}>
+                    {tekst}
+                </UserChildren>
+                <button onClick={this.uvecajGodine}>Uvecaj godine</button>
+            </>
+        );
+    }
 }
 
-export default App
+export default App;
